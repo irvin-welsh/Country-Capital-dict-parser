43ca64d0085f7970b27e1f5224e698a6facaf8e2
irvin-welsh <99cryptoman@gmail.com>
2020-04-29 10:38:47 +0300

    small improvements for better usability

---
+++ country_capitals_parser.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+import requests
+result_dict={}
+xpaths=[]
+
+driver = webdriver.Chrome('/bin/chromedriver')  # Optional argument, if not specified will search pathself.
+URL="https://www.thoughtco.com/capitals-of-every-independent-country-1434452"
+driver.get(URL)
+time.sleep(2) # Let the user actually see something!
+
+def get_all_xpaths():
+    for li_i in range(1,197):
+        xpath = "/html/body/main/article/div[4]/div[1]/div[7]/ol/li[{}]".format(li_i)
+        xpaths.append(xpath)
+        li_i+=1
+    return xpaths
+
+def convert_list_to_dict(data):
+    country_capitals=[]
+    for data in range(len(xpaths)):
+        capitals = driver.find_element_by_xpath(str(xpaths[data])).text
+        # capitals.text
+        capitals.split(":")
+        country_capitals.append(capitals)
+    print(country_capitals,len(country_capitals))
+    results_file = open("parsed.txt", 'a')
+    results_file.write(str(country_capitals)+'')
+    results_file.close()
+
+get_all_xpaths()
+convert_list_to_dict(xpaths)
