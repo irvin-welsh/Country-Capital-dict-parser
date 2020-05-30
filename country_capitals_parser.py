aba69b2d37af537a7f5ad926ccc2f13cd598a517
irvin-welsh <99cryptoman@gmail.com>
2020-05-30 11:51:53 +0300

    .

--- country_capitals_parser.py
+++ country_capitals_parser.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 import requests
+
 xpaths=[] # create an empty list to store all pointers to data we're looking for
 
 driver = webdriver.Chrome('/bin/chromedriver')  # initialize webdriver
@@ -27,7 +28,7 @@ def getCapitalsList(data): # function recieves xpaths[] list and parse its data 
     return country_capitals
 
 
-    
+
 getAllXpaths()
 getCapitalsList(xpaths)
 driver.quit()               # closing webdriver (browser)
